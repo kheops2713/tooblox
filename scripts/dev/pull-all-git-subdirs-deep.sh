1f1f681f5c3ebb6ee433b6ed96bc28024f9f00db
KheOps <kheops@ceops.eu>
2014-04-29 21:05:26 +0200

    Bugfix: directory changing was messed with the 'deep' version of git-pull script. Now uses --git-dir and --work-tree options.
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/dev/pull-all-git-subdirs-deep.sh
+++ scripts/dev/pull-all-git-subdirs-deep.sh
@@ -9,6 +9,13 @@ if [ ! -t 0 ]; then
 fi
 
 find "$DIR" ! -name .git ! -path '*/.git/*' -type d \
-  | while read MAYBE; do cd "$MAYBE" && git rev-parse --show-toplevel 2>/dev/null; done | sort | uniq \
-  | while read REPO; do echo "--- $REPO ---" && cd "$REPO" && git pull $PULL_OPTS && git pull --tags; echo "---"; done
+  | while read MAYBE; do if [ -d "$MAYBE/.git" ]; then
+      git --work-tree "$MAYBE" --git-dir "$MAYBE/.git" rev-parse --show-toplevel 2>/dev/null
+      fi
+    done | sort | uniq \
+  | while read REPO; do
+      echo "--- $REPO ---"
+      git --work-tree "$REPO" --git-dir "$REPO/.git" pull $PULL_OPTS && git --work-tree "$REPO" --git-dir "$REPO/.git" pull --tags
+      echo "---"
+    done
 
