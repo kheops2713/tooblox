451ed37b831fbe2d1693be237801e9ad22205685
KheOps <kheops@ceops.eu>
2015-05-20 17:25:01 +0200

    dmenu-bind script: possibility to specify an argument as command prefix to execute (typically for torify)
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/dmenu/dmenu-bind.sh
+++ scripts/dmenu/dmenu-bind.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-exe=`dmenu_path | dmenu -b -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617'` && eval "exec $exe"
+if [ -n "$1" ]; then
+  exe=`dmenu_path | dmenu -b -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617'` && eval "exec $1 $exe"
+else
+  exe=`dmenu_path | dmenu -b -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617'` && eval "exec $exe"
+fi
