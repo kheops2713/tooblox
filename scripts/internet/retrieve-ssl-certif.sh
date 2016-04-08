dc31eab809561daba099a1c6515c5f4cd814f814
KheOps <kheops@ceops.eu>
2016-04-08 12:30:29 -0400

    Add -servername for SNI
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/internet/retrieve-ssl-certif.sh
+++ scripts/internet/retrieve-ssl-certif.sh
@@ -6,6 +6,6 @@ REMHOST=$1
 REMPORT=${2:-443}
 
 echo |\
-openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |\
+openssl s_client -connect ${REMHOST}:${REMPORT} -servername ${REMHOST} 2>&1 |\
 sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
 
