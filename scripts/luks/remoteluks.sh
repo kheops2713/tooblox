1ebde7cdbc8c5127d13495629f0b8990936e314b
KheOps <kheops@ceops.eu>
2017-01-30 22:03:35 +0100

    Add usage indication if no argument given
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/luks/remoteluks.sh
+++ scripts/luks/remoteluks.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+REMOTEHOST=$1
+
+if [ -z "$REMOTEHOST" ]; then
+  echo Usage: $0 \<remote_host\> >&2
+  exit 1
+fi
+
 read -r -p "Passphrase: " -s pw
 echo -n "$pw" | ssh -o UserKnownHostsFile=~/.ssh/known_hosts.initramfs "root@$1"
