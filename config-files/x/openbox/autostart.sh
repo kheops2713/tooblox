f6ec43b9f4dfceb052ba8b27055e978d9c13eaff
KheOps <kheops@ceops.eu>
2019-11-06 20:28:07 +0100

    Add various autostart commands I use
    Signed-off-by: KheOps <kheops@ceops.eu>

--- config-files/x/openbox/autostart.sh
+++ config-files/x/openbox/autostart.sh
@@ -1,4 +1,7 @@
 tint2 &
 redshift &
 xautolock -locker /usr/bin/slock -time 3 &
+/usr/lib/notify-osd-customizable/notify-osd &
+syndaemon -R -t -i 0.7 -k -d
+synclient TapButton1=1 TapButton2=2 TapButton3=3
 urxvt -bl &
