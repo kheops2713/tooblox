aa68702795b66e47f070bc6d73ae075ead069cf0
KheOps <kheops@ceops.eu>
2020-12-27 21:03:33 +0100

    Script dmenu nmcli
    Signed-off-by: KheOps <kheops@ceops.eu>

---
+++ scripts/dmenu/dmenu-nmcli-wifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+bssid=Rafraichir
+
+while [ "$bssid" = "Rafraichir" ]; do
+  scan=$(nmcli -g bssid,freq,bars,in-use,security,ssid device wifi list | \
+    perl -pe \
+    's/^([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2}):([0-9]* MHz):([▂▄▆█_]*):([ \*]):([A-Za-z0-9 ]*):(.*)/$1:$2:$3:$4:$5:$6  $11($7)  $8  $10  $9/' ; echo Rafraichir)
+
+  sel=$(echo "$scan" | \
+        dmenu -l 10 -c \
+          -nb '#000000' \
+          -nf '#aaaaaa' \
+          -sb '#000000' \
+          -sf '#00ff00' \
+          -fn 'Monospace-12')
+
+  bssid=$(echo "$sel" | cut -d' ' -f1)
+done
+
+if [ -n "$bssid" ]; then
+  nmcli device wifi connect "$bssid"
+fi
