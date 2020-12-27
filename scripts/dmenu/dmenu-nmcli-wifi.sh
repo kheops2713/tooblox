60c82e35614b637ccb639dacd31351379a05994c
KheOps <kheops@ceops.eu>
2020-12-27 23:36:12 +0100

    dmenu wifi : force la variable TERM pour que nmcli soit plus joli et commentaire concernant dmenu
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/dmenu/dmenu-nmcli-wifi.sh
+++ scripts/dmenu/dmenu-nmcli-wifi.sh
@@ -2,11 +2,16 @@
 
 bssid=Rafraichir
 
+# This to convince nmcli it can display bars instead of starts for signal
+# strength
+export TERM=xterm-256color
+
 while [ "$bssid" = "Rafraichir" ]; do
   scan=$(nmcli -g bssid,freq,bars,in-use,security,ssid device wifi list | \
     /usr/bin/perl -pe \
     's/^([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2}):([0-9]* MHz):([▂▄▆█_\* ]*):([ \*]):([A-Za-z0-9 ]*):(.*)/$1:$2:$3:$4:$5:$6  $11($7)  $8  $10  $9/' ; echo Rafraichir)
 
+# Note: -c option of dmenu is only present in some (patched) versions of it
 
   sel=$(echo "$scan" | \
         dmenu -l 10 -c \
