643358475f504027edce47593377c250a153bf69
KheOps <kheops@ceops.eu>
2020-12-27 16:36:01 +0100

    typo
    Signed-off-by: KheOps <kheops@ceops.eu>

--- config-files/x/openbox/autostart.sh
+++ config-files/x/openbox/autostart.sh
@@ -11,4 +11,4 @@ xautolock -locker /usr/bin/slock -time 3 &
 /usr/lib/notify-osd-customizable/notify-osd &
 syndaemon -R -t -i 0.7 -k -d
 synclient TapButton1=1 TapButton2=2 TapButton3=3
-xfce4-terminal --maximize
+xfce4-terminal --maximize &
