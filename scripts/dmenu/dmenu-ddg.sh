23727287bfbf9877ed3336365fc3a018d3a61d75
KheOps <kheops@ceops.eu>
2020-12-29 15:30:20 +0100

    Change approach for dmenu-ddg, use X tools to give focus to tor browser; using remote from command line creates confusion with classic Firefox
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/dmenu/dmenu-ddg.sh
+++ scripts/dmenu/dmenu-ddg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BROUTEUR="/home/kheops/apps/tor-browser_en-US/Browser/start-tor-browser --detach --allow-remote"
 CACHE="/home/kheops/.cache/dmenu_web_search"
+BROUTEUR="Tor Browser"
 
 q=$(sort -u $CACHE | dmenu -l 10 -c -fn "Serif-14")
 
@@ -11,23 +11,8 @@ fi
 
 echo "$q" >>$CACHE
 
-url=$(echo "$q" | python3 -c '
-import requests
-from bs4 import BeautifulSoup
-from sys import stdin
-from urllib.parse import urlencode
-q=""
-for line in stdin:
-  q += "%s\n" % line
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
-}
-page = requests.get("https://duckduckgo.com/html/?%s" % urlencode({"q": q, "country": "france"}), headers=headers).text
-soup = BeautifulSoup(page, "html.parser").find_all("a", class_="result__url", href=True)
-for link in soup:
-    print(link["href"])' \
- | dmenu -l 5 -c -fn "Monospace-13")
-
-if [ -n "$url" ]; then
-  $BROUTEUR "$url"
-fi
+wmctrl -a "$BROUTEUR" && \
+  sleep 0.5 && \
+  xdotool key --delay 50 Ctrl+t Ctrl+l && \ 
+  xdotool type "$q" && \
+  xdotool key Return
