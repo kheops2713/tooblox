6a65e607f9bd6a6c038fa8293d4d122a403cdda4
KheOps <kheops@ceops.eu>
2020-12-31 10:07:21 +0100

    Fix regex for wireles security
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/dmenu/dmenu-nmcli-wifi.sh
+++ scripts/dmenu/dmenu-nmcli-wifi.sh
@@ -9,7 +9,7 @@ export TERM=xterm-256color
 while [ "$bssid" = "Rafraichir" ]; do
   scan=$(nmcli -g bssid,freq,bars,in-use,security,ssid device wifi list | \
     /usr/bin/perl -pe \
-    's/^([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2}):([0-9]* MHz):([▂▄▆█_\* ]*):([ \*]):([A-Za-z0-9 ]*):(.*)/$1:$2:$3:$4:$5:$6  $11($7)  $8  $10  $9/' ; echo Rafraichir)
+    's/^([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2})\\:([0-9A-F]{2}):([0-9]* MHz):([▂▄▆█_\* ]*):([ \*]):([A-Za-z0-9 .]*):(.*)/$1:$2:$3:$4:$5:$6  $11($7)  $8  $10  $9/' ; echo Rafraichir)
 
 # Note: -c option of dmenu is only present in some (patched) versions of it
 
