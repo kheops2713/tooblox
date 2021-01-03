a34ad551058fe957b15ca0e0260fb9dc2c627039
KheOps <kheops@ceops.eu>
2021-01-03 20:51:47 +0100

    Typo and more info
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/xorg-utils/url_from_firefox_in_terminal.sh
+++ scripts/xorg-utils/url_from_firefox_in_terminal.sh
@@ -3,8 +3,10 @@
 TERMWIN="xfce4-terminal"
 CMD="url"
 
+# Use with url_externalapp firefox extension
+
 # Idiotic parsing of data sent by Firefox (see
-# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#App_sideà
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#App_side)
 t=$(cat | dd bs=1 skip=4 | sed 's/^"//;s/"$//')
 
 xdotool search --onlyvisible xfce4-terminal windowactivate && \
