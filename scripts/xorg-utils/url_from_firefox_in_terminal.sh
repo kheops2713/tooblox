1a9a6e3e533bfc9000cc115a490ab6d8b0202589
KheOps <kheops@ceops.eu>
2021-01-03 20:46:14 +0100

    Commentaire et réduction de délai
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/xorg-utils/url_from_firefox_in_terminal.sh
+++ scripts/xorg-utils/url_from_firefox_in_terminal.sh
@@ -3,10 +3,12 @@
 TERMWIN="xfce4-terminal"
 CMD="url"
 
+# Idiotic parsing of data sent by Firefox (see
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#App_sideà
 t=$(cat | dd bs=1 skip=4 | sed 's/^"//;s/"$//')
 
 xdotool search --onlyvisible xfce4-terminal windowactivate && \
-  sleep 0.8 && \
+  sleep 0.2 && \
   xdotool key Ctrl+Shift+t && \
   sleep 0.2 && \
   xdotool type --delay 15 "$CMD $u" && \
