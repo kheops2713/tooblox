55f6f4cc241492c8322ef9e1342556150469f58b
KheOps <kheops@ceops.eu>
2021-01-03 20:17:38 +0100

    Délai de frappe à 15
    Signed-off-by: KheOps <kheops@ceops.eu>

--- scripts/xorg-utils/clipboard_url_terminal.sh
+++ scripts/xorg-utils/clipboard_url_terminal.sh
@@ -9,5 +9,5 @@ xdotool search --onlyvisible xfce4-terminal windowactivate && \
   sleep 0.8 && \
   xdotool key Ctrl+Shift+t && \
   sleep 0.2 && \
-  xdotool type --delay 20 "$CMD $u" && \
+  xdotool type --delay 15 "$CMD $u" && \
   xdotool key Return
