627c2ba79185e39e699599c19d1f06af859e88c9
KheOps <kheops@ceops.eu>
2021-03-03 22:24:24 +0100

    Changements cosmétiques à muttrc pour la barre d'état

--- config-files/mutt/gen-muttrc-accounts-macros.sh
+++ config-files/mutt/gen-muttrc-accounts-macros.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 MUTTRC_ACCOUNTS_FILES=~/.mutt/accounts-rc/*.muttrc
 
 BASE_IDX=6
 
 idx=$BASE_IDX
+fshortcuts=""
 for rcfile in $MUTTRC_ACCOUNTS_FILES; do
   echo "macro index <F${idx}> '<enter-command>source $rcfile<enter><change-folder>!<enter>'"
+  fshortcuts="${fshortcuts}F${idx}:$(basename $rcfile .muttrc) "
   idx=$(($idx+1))
 done
+
+echo "set my_fshortcuts=\"${fshortcuts%% }\""
