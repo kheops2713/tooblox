982b6b242ddb2ae0f200a2f80aa7b8fc05b5999d
KheOps <kheops@ceops.eu>
2023-12-28 22:19:48 +0100

    x509 sort

--- scripts/tls/display-x509-names.sh
+++ scripts/tls/display-x509-names.sh
@@ -8,7 +8,7 @@ while [ -n "$1" ]; do
     echo "* ${certfile} :"
     echo "  - CN : $(openssl x509 -in "$certfile" -noout -text  | sed '/^ *Subject:/!d;s/^.*CN = \([A-Za-z0-9.]*\).*/\1/')"
     echo "  - SAN :"
-    openssl x509 -in "$certfile" -noout -text | sed '/DNS:/!d;s/DNS://g;s/, /\n/g;s/^ */         /;s/\n/\n         /g'
+    openssl x509 -in "$certfile" -noout -text | sed '/DNS:/!d;s/DNS://g;s/, /\n/g;s/^ */         /;s/\n/\n         /g' | sort
   fi
   shift
 done
