494f2e4a067b4cc20af62774b0e2fba5ab27f5a5
KheOps <kheops@ceops.eu>
2023-12-28 22:16:51 +0100

    script affichage noms certificat x509

---
+++ scripts/tls/display-x509-names.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while [ -n "$1" ]; do
+  certfile=$1
+  if [ ! -r "$certfile" ]; then
+    echo "'$certfile' pas lisible"
+  else
+    echo "* ${certfile} :"
+    echo "  - CN : $(openssl x509 -in "$certfile" -noout -text  | sed '/^ *Subject:/!d;s/^.*CN = \([A-Za-z0-9.]*\).*/\1/')"
+    echo "  - SAN :"
+    openssl x509 -in "$certfile" -noout -text | sed '/DNS:/!d;s/DNS://g;s/, /\n/g;s/^ */         /;s/\n/\n         /g'
+  fi
+  shift
+done
