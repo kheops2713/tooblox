e255825fff8f02187dc8f1bd2eeb86c8791ffa5c
KheOps <kheops@ceops.eu>
2024-03-30 13:28:34 +0100

    retrieve ssl certif

--- scripts/internet/retrieve-ssl-certif.sh
+++ scripts/internet/retrieve-ssl-certif.sh
@@ -6,6 +6,6 @@ REMHOST=$1
 REMPORT=${2:-443}
 
 echo |\
-openssl s_client -connect ${REMHOST}:${REMPORT} -servername ${REMHOST} 2>&1 #|\
-#sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
+openssl s_client -connect ${REMHOST}:${REMPORT} -servername ${REMHOST} 2>&1 |\
+sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
 
